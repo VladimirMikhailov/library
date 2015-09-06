a73f2f8787b0a95de221a53a3d610c96dadeddae
Vladimir Mikhailov <vladimir.mikhailov@flatstack.com>
2015-09-06 12:45:02 +0300

    Add logic for pagination desc direction preloaded items

--- app/query_objects/library/paginatable_query/asc_preloaded.rb
+++ app/query_objects/library/paginatable_query/asc_preloaded.rb
@@ -1,19 +1,6 @@
 module Library
   class PaginatableQuery
-    class AscPreloaded
-      attr_reader :collection, :per_page, :last_seen_id
-      private :collection, :per_page, :last_seen_id
-
-      delegate :klass, to: :collection
-      delegate :first, to: :all
-      delegate :id, to: :first, prefix: true, allow_nil: true
-
-      def initialize(collection, per_page:, last_seen_id:)
-        @collection = collection
-        @per_page = per_page.to_i
-        @last_seen_id = last_seen_id.to_i
-      end
-
+    class AscPreloaded < BasePreloaded
       def all
         klass.with(
           previous: previous,
@@ -21,19 +8,7 @@ module Library
         ).from(union)
       end
 
-      def next?
-        all.length > per_page + indent
-      end
-
-      def previous?
-        first_id.to_i < last_seen_id
-      end
-
       private
-
-      def indent
-        previous? ? 1 : 0
-      end
 
       def previous
         klass.where("id <= ?", last_seen_id).order("id DESC").limit(1)
--- spec/query_objects/library/paginatable_query/desc_preloaded_spec.rb
+++ spec/query_objects/library/paginatable_query/desc_preloaded_spec.rb
@@ -1,11 +1,11 @@
 module Library
   class PaginatableQuery
-    describe AscPreloaded do
+    describe DescPreloaded do
       let(:per_page) { 2 }
-      let(:last_seen_id) { 0 }
-      let(:collection) { 3.times.map { |i| double(id: i + 1) } }
+      let(:last_seen_id) { 2 }
+      let(:collection) { Book.where("id <= ?", last_seen_id).order("id DESC").limit(per_page) }
 
-      let(:asc_preloaded) do
+      let(:desc_preloaded) do
         described_class.new(
           collection,
           per_page: per_page,
@@ -14,22 +14,19 @@ module Library
       end
 
       describe "#all" do
-        let(:collection) { Book.all }
-
         context "when it is next element" do
           let!(:collection_with_preloaded) { 3.times.map { |i| create(:book, id: i + 1) } }
 
-          before { create(:book) }
+          before { create(:book, id: 4) }
 
           it "returns records with preloaded next" do
-            expect(asc_preloaded.all).to eq_records(collection_with_preloaded)
+            expect(desc_preloaded.all).to eq_records(collection_with_preloaded)
           end
         end
 
         context "when it is both next and previous elements" do
-          let(:collection) { Book.where("id > ?", last_seen_id).limit(per_page) }
           let!(:collection_with_preloaded) { (2..5).map { |i| create(:book, id: i) } }
-          let(:last_seen_id) { 2 }
+          let(:last_seen_id) { 4 }
 
           before do
             create(:book, id: 1)
@@ -37,7 +34,7 @@ module Library
           end
 
           it "returns records with preloaded next and prev" do
-            expect(asc_preloaded.all).to eq_records(collection_with_preloaded)
+            expect(desc_preloaded.all).to eq_records(collection_with_preloaded)
           end
         end
       end
